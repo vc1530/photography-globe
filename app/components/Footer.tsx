4511a82b15dcad3e97f53a4bb7c710dbe07a7f90
vc1530 <90410748+vc1530@users.noreply.github.com>
2025-12-29 10:40:31 -0500

    footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -7,7 +7,7 @@ import { siteConfig } from "@/config/site";
 export function Footer() {
   const { place } = useAppContext();
   return (
-    <footer className="h-10 fixed bottom-0 left-0 w-full md:text-xl z-50">
+    <footer className="h-10 fixed bottom-0 left-0 w-full md:text-xl z-50 bg-inherit pt-2">
       <span className="flex font-fira absolute left-1/2 transform -translate-x-1/2 gap-4">
         <LocalTime
           timeZone={place.timeZone ?? siteConfig.common.homeTimeZone}
